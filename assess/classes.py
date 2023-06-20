6b3caac1ac131a8e1ddb6d66a811cc9f64643ed4
mailb0xhead <wood.douglas@gmail.com>
2023-06-20 09:16:02 -0400

    Next update

--- assess/classes.py
+++ assess/classes.py
@@ -116,3 +116,91 @@ class engage:
 
     def __str__(self):
         return self.name
+    
+########################################################################
+#### GOALS
+#### This class is used to store the goals
+########################################################################
+emptyGoal = {'goal_id':-1,'goal':'No Goal Defined','duration':'XX','priority':'-1'}
+
+class task:
+    def __init__(self, data):
+        self.id = data['goal_id']
+        self.name = data['goal']
+        self.duration = data['duration']
+        self.priority = data['priority']
+
+    def __str__(self):
+        return self.name
+    
+class st_goal:
+    def __init__(self, data):
+        self.id = data['goal_id']
+        self.name = data['goal']
+        self.duration = data['duration']
+        self.priority = data['priority']
+        self.tasks = []
+
+    def add_task(self, data):
+        for item in data:
+            self.tasks.append(task(item))
+
+    def __str__(self):
+        return self.name
+    
+class mt_goal:
+    def __init__(self, data):
+        self.id = data['goal_id']
+        self.name = data['goal']
+        self.duration = data['duration']
+        self.priority = data['priority']
+        self.st_goals = []
+
+    def add_st_goal(self, data):
+        for item in data:
+            self.st_goals.append(st_goal(item))
+
+    def __str__(self):
+        return self.name
+    
+class lt_goal:
+    def __init__(self, data):
+        self.id = data['goal_id']
+        self.name = data['goal']
+        self.duration = data['duration']
+        self.priority = data['priority']
+        self.mt_goals = []
+
+    def add_mt_goal(self, data):
+        for item in data:
+            self.mt_goals.append(mt_goal(item))
+
+    def __str__(self):
+        return self.name
+    
+class ll_goal:
+    def __init__(self, data):
+        self.id = data['goal_id']
+        self.name = data['goal']
+        self.duration = data['duration']
+        self.priority = data['priority']
+        self.lt_goals = []
+
+    def add_lt_goal(self, data):
+        for item in data:
+            self.lt_goals.append(lt_goal(item))
+
+    def __str__(self):
+        return self.name
+
+
+class goal:
+    def __init__(self, data):
+        self.ll_goals = []
+        if not data:
+            self.ll_goals.append(ll_goal(emptyGoal))
+        for item in data:
+            self.ll_goals.append(ll_goal(item))
+        
+    def __str__(self):
+        return self.name
--- assess/urls.py
+++ assess/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('question', views.question, name='question'),
     path('goals', views.goals, name='goals'),
     path('question/', apiviews.question, name='question'),
+    path('profile',views.profile, name='profile' ),
     path('goalapi/', apiviews.goals_api, name='goals'),
     path('ai_old/', views.ai, name='ai'),
     path('engagement/', views.engagement, name='engagement'),
